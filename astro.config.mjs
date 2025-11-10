877617f0694d31e14be749a97331ca171c1ebd9b
Artem Deikun <artemhp@gmail.com>
2025-11-10 17:01:17 -0500

    build: configure base path for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://artemhp.github.io/js-eras',
+	base: '/js-eras',
+	trailingSlash: 'never'
+});
